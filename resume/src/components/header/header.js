d2bdfdda518b608d88dca139e2962e57b65a8e5c
Conrad Rodgers <conrad13500@gmail.com>
2020-05-27 18:10:39 -0700

    first push

---
+++ resume/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {/*generated code*/}
+        <header id="home">....</header>
+      </React.Fragment>
+    );
+  }
+}
